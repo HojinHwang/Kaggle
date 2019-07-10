4a9f3c21b7f656d8b1638f8297c6f98facabf32b
HojinHwang <hojin1223@naver.com>
2019-07-10 19:26:48 +0900

    add file

---
+++ porto/porto_analysis_practice(1).R
@@ -0,0 +1,126 @@
+# Porto Seguro
+
+# general visualisation
+library('ggplot2')
+library('scales')
+library('grid')
+# install.packages('ggthemes')
+library('ggthemes')
+library('gridExtra')
+library('RColorBrewer')
+library('corrplot')
+
+# general data manipulation
+library('dplyr') # data manipulation
+library('readr') # input / output
+library(data.table) # data manipulation
+library(tibble) # data wrangling
+library(tidyr) # data wrangling
+library(stringr) # string manipulation
+library(forcats) # factor manipulation
+library(rlang) # data manipulation
+
+# specific visualisation
+library(alluvial)
+library(ggrepel)
+library(ggridges)
+library(VIM) # NAs
+library(plotly)
+library(ggforce)
+
+# modeling
+library(xgboost)
+library(caret)
+library(MLmetrics)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
