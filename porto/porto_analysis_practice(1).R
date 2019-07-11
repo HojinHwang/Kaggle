0e3385307ee2e5b9595db29830ce061526e7738a
HwangHoJin <41909501+HojinHwang@users.noreply.github.com>
2019-07-11 23:40:05 +0900

    Update porto_analysis_practice(1).R

--- porto/porto_analysis_practice(1).R
+++ porto/porto_analysis_practice(1).R
@@ -33,6 +33,44 @@ library(xgboost)
 library(caret)
 library(MLmetrics)
 
+# Define multiple plot function
+multiplot <- function(..., plotlist = NULL, file, cols = 1, layout = NULL){
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if(is.null(layout)){
+    # Make the panel
+    # ncol : Number of columns of plots
+    # nrow : Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots / cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if(numPlots == 1){
+    print(plots[[1]])
+  }
+  else{
+    # Set up the page
+  grid.newpage()
+  pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+  
+  # Make each plot, in the correct location
+  for(i in 1:numPlots){
+    # Get the i, j  matrix positions of the regions that contains this subplot
+    matchidx <- as.data.frame(which(layout == i, arr.ind = T))
+    
+    print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                    layout.pos.col = matchidx$col))
+  }
+  
+}
+}
+
+# function to extract binomial confidence levels
+get_binCI <- function(x, n) as.list(setNames(binom.test(x, n)$conf.int, c("lwr", "upr")))
 
 
 
