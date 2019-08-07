459e69903a7b6fcdf81c15020854f269a44bc838
HojinHwang <hojin1223@naver.com>
2019-08-07 23:29:34 +0900

    Costa rican household Poverty Levels Predict

---
+++ costa rican household/Predicting Poverty Levels(1).R
@@ -0,0 +1,16 @@
+# Costa Rican Household Poverty Level Prediction : 코스타리카 가계 빈곤 수준 예측
+library(tidyverse) # importing, cleaning, visualising
+library(xgboost) # gradient boosted machines
+library(caret) # ml pipelines made easy
+library(VIM) # missing data
+library(corrplot) # correlation visualisation
+library(ggthemes) # extra plotting themes
+library(plotly) # interactive plots 
+library(factoextra) # pca visualisation
+library(Rtsne) # visualisation of high dimensial datsets
+# library(largeVis) # visualisation of high dimensial datsets
+library(breakDown) # visualising model results
+library(gridExtra) # arranging plots
+# library(lightgbm) # leaf-wise gbm
+library(glmnet) # regularised regression
+
